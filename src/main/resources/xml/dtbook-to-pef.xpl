61c57a81c80128c969a86eaea32df4bc6f53316f
Bert Frees <bertfrees@gmail.com>
2015-09-18 21:22:36 +0200

    Bare bones of a custom dtbook-to-pef script

---
+++ src/main/resources/xml/dtbook-to-pef.xpl
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<p:declare-step type="mtm:dtbook-to-pef" version="1.0"
+                xmlns:mtm="http://www.mtm.se/pipeline/"
+                xmlns:p="http://www.w3.org/ns/xproc"
+                xmlns:px="http://www.daisy.org/ns/pipeline/xproc"
+                xmlns:dotify="http://code.google.com/p/dotify/"
+                xmlns:pef="http://www.daisy.org/ns/2008/pef"
+                exclude-inline-prefixes="#all"
+                name="main">
+    
+    <p:input port="source" primary="true" px:name="source" px:media-type="application/x-dtbook+xml"/>
+    
+    <p:option name="include-preview" required="false" px:type="boolean" select="''"/>
+    <p:option name="include-brf" required="false" px:type="boolean" select="''"/>
+    <p:option name="output-dir" required="true" px:output="result" px:type="anyDirURI"/>
+    
+    <p:import href="http://www.daisy.org/pipeline/modules/braille/dotify-utils/library.xpl"/>
+    <p:import href="http://www.daisy.org/pipeline/modules/braille/pef-utils/library.xpl"/>
+    
+    <dotify:xml-to-obfl locale="sv-SE" dotify-options="(rows:29)(cols:28)(rowgap:4)"/>
+    
+    <dotify:obfl-to-pef locale="sv-SE" mode="uncontracted"/>
+    
+    <pef:store>
+        <p:with-option name="output-dir" select="$output-dir"/>
+        <p:with-option name="name" select="replace(p:base-uri(/),'^.*/([^/]*)\.[^/\.]*$','$1')">
+            <p:pipe step="main" port="source"/>
+        </p:with-option>
+        <p:with-option name="include-preview" select="$include-preview"/>
+        <p:with-option name="include-brf" select="$include-brf"/>
+    </pef:store>
+    
+</p:declare-step>
--- src/test/java/MTMTest.java
+++ src/test/java/MTMTest.java
@@ -5,15 +5,20 @@ import java.io.FilenameFilter;
 
 import org.daisy.maven.xproc.xprocspec.XProcSpecRunner;
 
+import org.daisy.pipeline.braille.dotify.DotifyTranslator;
+
 import static org.daisy.pipeline.pax.exam.Options.brailleModule;
+import static org.daisy.pipeline.pax.exam.Options.calabashConfigFile;
 import static org.daisy.pipeline.pax.exam.Options.domTraversalPackage;
 import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
 import static org.daisy.pipeline.pax.exam.Options.forThisPlatform;
 import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
 import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.pipelineModule;
 import static org.daisy.pipeline.pax.exam.Options.thisBundle;
 import static org.daisy.pipeline.pax.exam.Options.xprocspecBundles;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,7 +37,17 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class TranslationTest {
+public class MTMTest {
+	
+	@Inject
+	private DotifyTranslator.Provider provider;
+	
+	@Test
+	public void testTranslation() {
+		DotifyTranslator translator = provider.get("(locale:sv_SE)").iterator().next();
+		assertEquals("⠠⠙⠑⠞⠀⠜⠗⠀⠥⠝⠙⠑⠗⠀⠍⠕⠗⠛⠕⠝⠍⠪⠞⠑⠞⠀⠙⠑⠞⠀⠓⠜⠝⠙⠑⠗⠄⠀⠠⠍⠁⠗⠊⠁⠀⠓⠡⠇⠇⠑⠗⠀⠚⠥⠎⠞⠀⠏⠡⠀⠁⠞⠞⠀⠇⠜⠎⠁⠀⠥⠏⠏⠀⠑⠝⠀⠗⠁⠏⠏⠕⠗⠞⠄",
+		             translator.transform("Det är under morgonmötet det händer. Maria håller just på att läsa upp en rapport."));
+	}
 	
 	@Inject
 	private XProcSpecRunner xprocspecRunner;
@@ -53,6 +68,7 @@ public class TranslationTest {
 		return options(
 			logbackConfigFile(),
 			logbackBundles(),
+			calabashConfigFile(),
 			domTraversalPackage(),
 			felixDeclarativeServices(),
 			mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
@@ -61,10 +77,17 @@ public class TranslationTest {
 			mavenBundle().groupId("org.daisy.braille").artifactId("braille-css").versionAsInProject(),
 			mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.antlr-runtime").versionAsInProject(),
 			mavenBundle().groupId("com.googlecode.texhyphj").artifactId("texhyphj").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.pef-tools").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.braille").artifactId("braille-utils.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.libs").artifactId("jing").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.api").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.common").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.translator.impl").versionAsInProject(),
 			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.hyphenator.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.formatter.impl").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.task-api").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.dotify").artifactId("dotify.task.impl").versionAsInProject(),
 			brailleModule("common-utils"),
 			brailleModule("css-core"),
 			brailleModule("css-calabash"),
@@ -73,6 +96,12 @@ public class TranslationTest {
 			brailleModule("dotify-saxon"),
 			brailleModule("dotify-calabash"),
 			brailleModule("dotify-utils"),
+			brailleModule("pef-core"),
+			brailleModule("pef-calabash"),
+			brailleModule("pef-saxon"),
+			brailleModule("pef-utils"),
+			pipelineModule("common-utils"),
+			pipelineModule("file-utils"),
 			thisBundle(),
 			xprocspecBundles(),
 			junitBundles()
